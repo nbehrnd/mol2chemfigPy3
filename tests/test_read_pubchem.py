204b99da4c2a1f00d1b74354f29bd4235065ccc3
Norwid Behrnd <nbehrnd@yahoo.com>
2026-07-21 17:18:36 +0200

    ci(pytest.yml): exclude parallelism in CI
    The matrix approach to test over multiple python interpreters on
    one, and multiple os.runners on the other shall be retained --
    already for the ease to maintain and update the workflow in future.
    Yet by `max-parallel: 1` added, parallelism recognized as dangerous
    for testing the fragile interaction with NIH/PubChem servers is
    prevented.
    
    At the same time, reduction of the pause between two requests is
    lowered, previously 10 s, now 3 s.
    
    Signed-off-by: Norwid Behrnd <nbehrnd@yahoo.com>

--- tests/test_read_pubchem.py
+++ tests/test_read_pubchem.py
@@ -8,7 +8,7 @@ _sys = platform.system()
 @pytest.fixture(autouse=True)
 def _throttle_pubchem():
     yield
-    time.sleep(10)
+    time.sleep(3)
 
 target1 = r"""\chemfig{H-[:210,0.62]-[:150](-[:90]O-[:30,0.62]H)(%
 -[:270,,,,draw=none]\mcfcringle{1.3})-[:210](-[:150,0.62]H)-[:270](%
